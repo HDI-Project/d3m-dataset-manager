87f3ee0ed13583c4596a94ad422760ce6df2cecd
Carles Sala <c.sala.stq@gmail.com>
2018-11-19 13:34:57 +0100

    Generate dataSplits.csv if it does not exist

--- dataset_manager/splitter.py
+++ dataset_manager/splitter.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import os
 import json
-from collections import OrderedDict
+import logging
 from io import StringIO
+from collections import OrderedDict
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+LOGGER = logging.getLogger(__name__)
+
+
+def to_csv(df):
+    buf = StringIO()
+    df.to_csv(buf, index=None)
+    return buf.getvalue().encode()
 
 
 def read_csv(dataset, csv_path):
@@ -15,10 +26,20 @@ def read_csv(dataset, csv_path):
     return pd.read_csv(StringIO(data.decode()))
 
 
-def get_split(ds, ld, label):
-    split_index = ds[ds['type'] == label].d3mIndex
-    ld_indexed = ld.set_index('d3mIndex')
-    split = ld_indexed.loc[split_index]
+def write_csv(df, dataset, csv_path):
+    folder = dataset
+    levels = csv_path.split('/')
+    for level in levels[:-1]:
+        folder = folder[level]
+
+    folder[levels[-1]] = to_csv(df)
+
+def get_split(data_splits, learning_data, label):
+    split_index = data_splits[data_splits['type'] == label].d3mIndex
+
+    learning_data_indexed = learning_data.set_index('d3mIndex')
+
+    split = learning_data_indexed.loc[split_index]
     return split.reset_index()
 
 
@@ -44,28 +65,57 @@ def get_target_names(problem_data, learning_data, dataset_doc):
     if all(target in learning_data for target in target_names):
         return target_names
 
-    print("WARNING: target names not found in learning data")
-    print("Falling back to using suggestedTarget roles")
+    LOGGER.warning("Target names not found in learning data")
+    LOGGER.warning("Falling back to using suggestedTarget roles")
 
-    def ld_filter(d):
-        return d['resPath'] == 'tables/learningData.csv'
+    def learning_data_filter(d):
+        return d['resPath'] ==  'tables/learningData.csv'
 
     resources = dataset_doc['dataResources']
-    ld_resource = list(filter(lambda d: d['resPath'] == 'tables/learningData.csv', resources))[0]
-    target_columns = filter(lambda d: 'suggestedTarget' in d['role'], ld_resource['columns'])
+    learning_data_resource = list(filter(
+        lambda d: d['resPath'] == 'tables/learningData.csv',
+        resources
+    ))[0]
+    target_columns = filter(
+        lambda d: 'suggestedTarget' in d['role'],
+        learning_data_resource['columns']
+    )
 
     return [column['colName'] for column in target_columns]
+
+
+def build_data_splits(learning_data):
+    train, test = train_test_split(learning_data, test_size=0.2, random_state=0)
+    splits = pd.DataFrame(index=learning_data['d3mIndex'])
+    splits.loc[train['d3mIndex'], 'type'] = 'TRAIN'
+    splits.loc[test['d3mIndex'], 'type'] = 'TEST'
+    splits['repeat'] = 0
+    splits['fold'] = 0
+
+    return splits.reset_index()
+
+
+def get_data_splits(dataset, dataset_problem, learning_data):
+    path = os.path.join(dataset_problem, 'dataSplits.csv')
+    try:
+        data_splits = read_csv(dataset, path)
+    except Exception:
+        LOGGER.warning('dataSplits.csv not found in %s. Generating one.', dataset_problem)
+        data_splits = build_data_splits(learning_data)
+        write_csv(data_splits, dataset, path)
+
+    return data_splits
 
 
 def get_dataset_split(full_dataset, dataset_name, label, problem, targets=False):
     problem_suffix = '_problem' + problem
 
     # get dataframes
-    ds = read_csv(full_dataset, dataset_name + problem_suffix + '/dataSplits.csv')
-    ld = read_csv(full_dataset, dataset_name + '_dataset/tables/learningData.csv')
+    learning_data = read_csv(full_dataset, dataset_name + '_dataset/tables/learningData.csv')
+    data_splits = get_data_splits(full_dataset, dataset_name + problem_suffix, learning_data)
 
     # get the learningData split
-    ld_split = get_split(ds, ld, label)
+    learning_data_split = get_split(data_splits, learning_data, label)
 
     # NOTE: Here we use a copy instead of a deepcopy to avoid duplicating
     # all the data inside the tables structure
@@ -78,7 +128,7 @@ def get_dataset_split(full_dataset, dataset_name, label, problem, targets=False)
 
     # Prepare the datasetDoc.json
     dataset_doc = json.loads(dataset_split['datasetDoc.json'].decode(),
-                             object_pairs_hook=OrderedDict)
+                         object_pairs_hook=OrderedDict)
     dataset_id = dataset_doc['about']['datasetID'] + suffix
     dataset_doc['about']['datasetID'] = dataset_id
 
@@ -92,7 +142,7 @@ def get_dataset_split(full_dataset, dataset_name, label, problem, targets=False)
 
     problem_data['datasetID'] = dataset_id
 
-    target_names = get_target_names(problem_data, ld, dataset_doc)
+    target_names = get_target_names(problem_data, learning_data, dataset_doc)
 
     dataset_split['datasetDoc.json'] = json.dumps(dataset_doc, indent=2).encode()
     problem_split['problemDoc.json'] = json.dumps(problem_doc, indent=2).encode()
@@ -103,18 +153,19 @@ def get_dataset_split(full_dataset, dataset_name, label, problem, targets=False)
     }
 
     if targets:
-        split['targets.csv'] = to_csv(ld_split[['d3mIndex'] + target_names])
+        split['targets.csv'] = to_csv(learning_data_split[['d3mIndex'] + target_names])
 
     if label == 'TEST':
         for target_name in target_names:
-            ld_split[target_name] = None
+            learning_data_split[target_name] = None
 
-    dataset_split['tables']['learningData.csv'] = to_csv(ld_split)
+    dataset_split['tables']['learningData.csv'] = to_csv(learning_data_split)
 
     return split
 
 
 def add_dataset_splits(dataset, dataset_name):
+    LOGGER.info('Adding dataset splits to %s', dataset_name)
     problems = get_problem_names(dataset, dataset_name)
     for problem in problems:
         dataset['TRAIN' + problem] = get_dataset_split(
